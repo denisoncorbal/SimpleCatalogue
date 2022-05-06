5fb7cdc5f9ee58e1179baac446948ca0c0f590b0
Denison G. Corbal <denison.corbal@gmail.com>
2022-05-06 15:34:30 -0300

    Base project generated from start.spring.io

---
+++ src/main/java/br/com/dgc/simplecatalogue/SimpleCatalogueApplication.java
@@ -0,0 +1,13 @@
+package br.com.dgc.simplecatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dgc/simplecatalogue/SimpleCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dgc.simplecatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
