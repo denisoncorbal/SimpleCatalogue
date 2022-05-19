6164c8370cbe4c8eec35c0846736cddc1286f69d
Denison G. Corbal <denison.corbal@gmail.com>
2022-05-19 11:20:14 -0300

    Improved book representation.

--- src/main/java/br/com/dgc/simplecatalogue/model/entity/Book.java
+++ src/main/java/br/com/dgc/simplecatalogue/model/entity/Book.java
@@ -1,7 +1,10 @@
 package br.com.dgc.simplecatalogue.model.entity;
 
+import java.util.List;
 import java.util.Set;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.Table;
 
 /**
@@ -11,13 +14,38 @@ import javax.persistence.Table;
  * @since 1.0
  */
 @Entity
-@Table(name = "tb_books")
+@Table(name = "tb_books", indexes = {
+    @Index(columnList = "authors"),
+    @Index(columnList = "isbn"),
+    @Index(columnList = "cdd")
+})
 public class Book extends Work {
+
+  private Set<String> authors;
+
+  @Column(length = 13)
+  private String isbn;
+
+  @Column(length = 8)
+  private String issn;
+
+  @Column(length = 3)
+  private String language;
+
+  @Column(length = 7)
+  private String cdd;
+
+
   public Book() {
     super();
   }
 
-  public Book(Long idWork, String name, Set<Copy> copies) {
+  public Book(Long idWork, String name, Set<Copy> copies, Set<String> authors, String isbn, String issn, String language, String cdd) {
     super(idWork, name, copies);
+    this.authors = authors;
+    this.isbn = isbn;
+    this.issn = issn;
+    this.language = language;
+    this.cdd = cdd;
   }
 }
